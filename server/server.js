1543274239d6138f6e9a0350b5225c4959cf2cf6
Philipp <philipp@the-pearl.local>
2013-03-12 04:09:23 +0100

    read port from process.env.PORT

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.get('*', function (req, res) {
 
 // Start server
 
-app.listen(9140, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
